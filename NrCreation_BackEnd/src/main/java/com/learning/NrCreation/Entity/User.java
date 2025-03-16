1b1b7698bafd2a97fbc30f2600b4911fa8f6e1c6
SandeepJaiswar2022 <sandy2662001@gmail.com>
2025-03-16 18:55:17 +0530

    16-03-25

--- NrCreation_BackEnd/src/main/java/com/learning/NrCreation/Entity/User.java
+++ NrCreation_BackEnd/src/main/java/com/learning/NrCreation/Entity/User.java
@@ -24,9 +24,7 @@ public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	
-	
+
 	private String firstName;
 	
 	private String lastName;
